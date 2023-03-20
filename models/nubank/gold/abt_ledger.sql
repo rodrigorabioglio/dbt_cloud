98d3e5aa8fab086b779cbc4021ddc91a06a18e0b
rodrigo.rabioglio@gmail.com <rodrigo.rabioglio@gmail.com>
2023-03-20 13:19:48 +0000

    changed abt from transactions to ledger

--- models/nubank/gold/abt_ledger.sql
+++ models/nubank/gold/abt_ledger.sql
@@ -1,27 +1,27 @@
 SELECT
-    transactions.id
-    ,transactions.account_id
+    ledger.id
+    ,ledger.account_id
     ,customer.customer_id
     ,requested_time.action_timestamp AS requested_at
     ,completed_time.action_timestamp AS completed_at
-    ,transactions.status
-    ,transactions.product
-    ,transactions.transaction_type
-    ,transactions.amount
+    ,ledger.status
+    ,ledger.product
+    ,ledger.transaction_type
+    ,ledger.amount
     ,account.account_branch
     ,account.account_number
     ,account.account_check_digit
     ,city.city AS customer_city
     ,city.state AS customer_state
     ,city.country AS customer_country
-FROM {{ ref('fact_transactions_p2p') }} AS transactions
+FROM {{ ref('fact_ledger') }} AS ledger
 JOIN {{ ref('dim_account') }} AS account
-    ON account.account_id = transactions.account_id
+    ON account.account_id = ledger.account_id
 JOIN {{ ref('dim_customer') }} AS customer
     ON customer.customer_id = account.customer_id
 JOIN {{ ref('dim_city') }} AS city
     ON customer.city_id = city.city_id
 JOIN {{ ref('dim_time') }} AS requested_time
-    ON requested_time.time_id = transactions.requested_at
+    ON requested_time.time_id = ledger.requested_at
 JOIN {{ ref('dim_time') }} AS completed_time
-    ON completed_time.time_id = transactions.requested_at
+    ON completed_time.time_id = ledger.requested_at
